40b7c1d9e566b79fa1b889ca0d1c32b2959d09dd
maloryware <malory@onenetbeyond.org>
2025-05-17 06:45:32 +0100

    fixed qmj & refactored build.gradle to accommodate gradle 8.10

--- src/main/java/com/example/example_mod/ExampleMod.java
+++ src/main/java/com/example/example_mod/ExampleMod.java
@@ -6,11 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
+
+	// FIXME: This WILL crash on startup due to a QFAPI specificity.
+	//  workaround: add launch arg -Dloader.workaround.disable_strict_parsing=true
+
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
-
 	@Override
 	public void onInitialize(ModContainer mod) {
 		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
